49ff1e9b7bac05e74ae2227ae0af52f9128a1018
Oli Hickie <oli.h@hotmail.co.uk>
2025-04-08 17:00:33 +0100

    Create page folders

---
+++ my-app/src/app/about/page.tsx
@@ -0,0 +1,15 @@
+
+const AboutPage = () => {
+  return (
+   <div>
+      <h1>About Me</h1>
+      <p>
+        Hello! I'm a front end developer with a passion for creating beautiful and
+        functional websites. I love working with the latest technologies and
+        continuously learning new skills.
+      </p>
+   </div>
+  );
+};
+
+export default AboutPage;
